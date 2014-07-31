02dc8e8ae3b68a0c72971e1409892eceafef839b
Egor <kartvep@gmail.com>
2014-07-31 17:40:40 +0400

    ErrCountAggregator.py - use juggler-client, correct return values. Some other fixes.

--- combaine/plugins/Aggregators/ErrCountAggregator.py
+++ combaine/plugins/Aggregators/ErrCountAggregator.py
@@ -6,9 +6,10 @@ from combaine.common.loggers import CommonLogger
 from collections import defaultdict
 
 import sys
-sys.path.append('/usr/lib')
-from jclient import config
-from jclient import jobs
+from subprocess import Popen, PIPE
+#sys.path.append('/usr/lib')
+#from jclient import config
+#from jclient import jobs
 
 class ErrCountAggregator(RawAbstractAggregator):
 
@@ -19,8 +20,8 @@ class ErrCountAggregator(RawAbstractAggregator):
 
         self.q_dict = {"http_host": config["request"]["http_host_field"],
                       "geturl" : "SUBSTRING_INDEX(%s, '?', 1)" % config["request"]["geturl_field"]}
-        self.q_host_url = "%(http_host)s, %(geturl)s" % self_q_dict
-        self.q_head = "SELECT %s, COUNT(*) from %TABLENAME%" % self.q_host_url
+        self.q_host_url = "%(http_host)s, %(geturl)s" % self.q_dict
+        self.q_head = "SELECT %s, COUNT(*) from %%TABLENAME%%" % self.q_host_url
         self.q_tail = "GROUP BY %s" % self.q_host_url
 
         mk_q_dict = lambda x: x.update(self.q_dict) or x
@@ -35,6 +36,7 @@ class ErrCountAggregator(RawAbstractAggregator):
         self.limits = dict([ (key, tuple(val)) for key, val in config["limits"].items() ])
 
     def _query(self, code=None):
+        '''Renders SQL queries.'''
         q = [self.q_head]
         if code or self.q_blacklist:
             q.append("WHERE")
@@ -47,21 +49,46 @@ class ErrCountAggregator(RawAbstractAggregator):
         return self.table_regex.sub(self.dg.tablename, q)
 
     def aggregate(self, host_name, timeperiod):
-        config.loadConfigs()
+        '''Doing the work.'''
+        #config.loadConfigs()
 
         def send_juggler(msg):
-            self.logger.debug("%s, %s" % (host_name, msg))
-            status = {0 : "OK", 1 : "WARN", 2 : "CRIT"}
-            st, dsc = msg
-            st = status[st]
-            self.logger.debug("%s %s %s %s" % (host_name, self.check_name, st, dsc))
-            if not jobs.addJobs(host_name, self.check_name, st, dsc):
-                self.logger.error("Can't send data to juggler")
+            self.logger.debug("%s, %s:%s" % (host_name, msg.state, msg.txt))
+            p = Popen(["/usr/bin/juggler_queue_event",
+                                   "--host", host_name,
+                                   "-s", str(msg.state),
+                                   "-n", self.check_name,
+                                   "-d", msg.txt,
+                                  ],
+                      stderr = PIPE, stdout = PIPE)
+            output = '; '.join(p.communicate()).replace('\n', '\\n')
+ 
+            if p.returncode != 0:
+                self.logger.error("send_jugler failed: %s -> %s" % (p.returncode, output))
+            else:
+                self.logger.info("sucessfully sent to juggler")
+
+
+        class Msg:
+            '''Juggler message class.
+               Contains the most critical data was stored. Drops less critical records'''
+            def __init__(self, state=0, txt="Ok"):
+                self.state = state
+                self.txt = txt
+    
+            def add (self, state, txt):
+                if state == self.state:
+                    self.txt += "; %s" % txt
+                elif state > self.state:
+                    self.state = state
+                    self.txt = txt
             
         db = self.dg
         rekey = lambda val: ((val[0],val[1]), val[2]) 
         reqs_all = dict(map(rekey, db.perfomCustomQuery(self._query())))
+        reqs_all["_TOTAL_"] = sum(reqs_all.values())
         reqs_err = dict(map(rekey, db.perfomCustomQuery(self._query(self.check_code))))
+        reqs_err["_TOTAL_"] = sum(reqs_err.values())
         err_prct = ( (handler,
                       total,
                       reqs_err.get(handler, 0), 
@@ -73,20 +100,21 @@ class ErrCountAggregator(RawAbstractAggregator):
         self.logger.debug("%s %s%%" % (host_name, 
                                      float(sum(reqs_err.values())) / sum(reqs_all.values()) * 100))
 
-        juggler_msg = (0, "Ok")
+        juggler_msg = Msg(0, "Ok")
+
         for handler, requests, errors, percents in err_prct:
             vhost = handler[0]
-            handler = ''.join(handler)
+            handler = "".join(handler)
             max_errs, min_reqs = self.limits.get(vhost, self._limits)
-            #print handler, percents, max_errs, requests, min_reqs
+
             if percents >= max_errs and requests <= min_reqs:
                 #warning
                 msg = "%s - %s%% (%s/%s)" % (handler, percents, errors, requests)
-                juggler_msg = (1, msg) if juggler_msg[0] < 2 else juggler_msg
+                juggler_msg.add(1, msg)
             elif percents >= max_errs and requests > min_reqs:
                 #critical
                 msg = "%s - %s%% (%s/%s)" % (handler, percents, errors, requests)
-                juggler_msg = (2, msg)
+                juggler_msg.add(2, msg)
 
             if errors:
                 # TODO
@@ -95,13 +123,19 @@ class ErrCountAggregator(RawAbstractAggregator):
 
         send_juggler(juggler_msg)
 
-    def _pack(self, data):
-        pass
+        #Just stub
+        return self.name, self._pack(None, timeperiod[0])
+
+    def _pack(self, data, time):
+        '''Just stub'''
+        return {'time':time, 'res':list(data)}
 
     def _unpack(self, data):
-        pass
+        '''Just stub'''
+        return None, None
    
     def aggregate_group(self, data):
+        '''Just stub'''
         raise StopIteration
 
 PLUGIN_CLASS = ErrCountAggregator
